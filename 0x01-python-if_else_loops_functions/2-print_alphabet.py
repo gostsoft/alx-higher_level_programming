462671e6c2cfe9b0d32afc4e26e748340ecb92c6
gostsoft <mrjohn.soft@gmail.com>
2022-06-28 20:24:58 +0000

    m

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-letters = "a b c d e f g h i j k l m n o p q r s t u v w x y z"
 
-for let in letters:
-    print(ascii(let))
+
+for let in range(97, 123):
+    print(f"{let:c}", end="")
 exit()
