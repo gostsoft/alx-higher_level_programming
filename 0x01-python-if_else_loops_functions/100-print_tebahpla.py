b9188694964eb11e30b73d781ea62c406faa5dd2
gostsoft <mrjohn.soft@gmail.com>
2022-06-29 20:38:13 +0000

    m

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 
+
 for num in reversed(range(97, 123)):
-    print("{:c}".format(num if num %2 ==0 else(num -32)), end="")
+    print("{:c}".format(num if num % 2 == 0 else(num - 32)), end="")
