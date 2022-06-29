e3571f53cceace841b0121bdfe4d60fbc35b84bd
gostsoft <mrjohn.soft@gmail.com>
2022-06-29 15:04:22 +0000

    m

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,7 +3,7 @@
 for num in range(0, 90):
     if num % 10 > num/10:
         if num !=89:
-            print("{0:02d},".format(num), end="")
+            print("{0:02d}, ".format(num), end="")
         else:
             print("{0:02d}".format(num))
 
