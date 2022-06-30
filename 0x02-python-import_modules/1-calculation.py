d90bd378267badcae8ca4539a7f1a840f5cdfbd3
gostsoft <101713427+gostsoft@users.noreply.github.com>
2022-06-30 21:45:34 +0100

    Update 1-calculation.py
    m

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,9 +2,9 @@
 if __name__ == "__main__":
     import calculator_1
 
-
     a = 10
     b = 5
+    
     print(f"{a} + {b} = {calculator_1.add(a, b):d}")
     print(f"{a} - {b} = {calculator_1.sub(a, b):d}")
     print(f"{a} * {b} = {calculator_1.mul(a, b):d}")
