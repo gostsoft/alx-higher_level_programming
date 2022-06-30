4bc20eeb5c278be20b2d53cbc8d4551eb60fd05b
gostsoft <101713427+gostsoft@users.noreply.github.com>
2022-06-30 21:44:16 +0100

    Update 1-calculation.py
    m

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
     b = 5
     print(f"{a} + {b} = {calculator_1.add(a, b):d}")
     print(f"{a} - {b} = {calculator_1.sub(a, b):d}")
-print(f"{a} * {b} = {calculator_1.mul(a, b):d}")
-print(f"{a} / {b} = {calculator_1.div(a, b):.0f}")
+    print(f"{a} * {b} = {calculator_1.mul(a, b):d}")
+    print(f"{a} / {b} = {calculator_1.div(a, b):.0f}")
