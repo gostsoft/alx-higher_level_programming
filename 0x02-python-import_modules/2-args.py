0b822e91b72200e58e048a1fe2f9897a4651870e
gostsoft <mrjohn.soft@gmail.com>
2022-07-01 14:53:32 +0000

    m

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,4 +5,5 @@ if __name__ == "__main__":
 
     print(f"{len(sys.argv)} argument:")
     for n in range(len(sys.argv)):
-        print(f"{n+1}: {sys.argv[n+1]}")
+        if n != 0:
+            print(f"{n}: {sys.argv[n]}")
