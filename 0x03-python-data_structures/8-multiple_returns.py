1e4b14e099bd141416354d33034fec179d9438b5
gostsoft <mrjohn.soft@gmail.com>
2022-07-04 11:59:13 +0000

    m

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,7 +2,6 @@
 
 def multiple_returns(sentence):
     lenght = len(sentence)
-    if len(sentence) == 0:
+    if (sentence) == "":
         return ("Lenght: {:d} - First character: {}".format(lenght, None))
-    else:
-         return ("Lenght: {:d} - First character: {}".format(lenght, sentence[0]))
+    return ("Lenght: {:d} - First character: {}".format(lenght, sentence[0]))
