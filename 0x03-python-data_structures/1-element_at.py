14f5a005b4424742ad2bf4bb1aff44f82d034901
gostsoft <mrjohn.soft@gmail.com>
2022-07-04 11:31:06 +0000

    m

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,7 +2,7 @@
 
 
 def element_at(my_list, idx):
-    if idx < 0 or idx > len(my_list) - 1:
+    if idx < 0 or idx >= len(my_list) - 1:
         return 'None'
     else:
         print("Element at index {:d} is {:d}".format(idx, my_list[idx]))
---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    if len(matrix[0]) <= 0:
+            print("$")
+    for m in matrix:
+        for i in m:
+            print("{:d}".format(i), end=" " if i != m[-1] else "$")
+        print()
