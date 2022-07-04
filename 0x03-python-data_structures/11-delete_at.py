0085431c209bb755112dab0d009de89b5927b160
gostsoft <mrjohn.soft@gmail.com>
2022-07-04 19:47:20 +0000

    m

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,7 +2,7 @@
 
 
 def delete_at(my_list=[], idx=0):
-    if idx < 0 or idx >len((my_list)):
+    if idx < 0 or idx > len((my_list)) - 1:
         return my_list
     new_list = [i for i in my_list if i != my_list[idx]]
     my_list.clear()
