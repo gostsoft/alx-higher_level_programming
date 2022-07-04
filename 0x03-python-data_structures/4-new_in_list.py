7768113bc04b0fe7844c65d6f2048a934c972f21
gostsoft <mrjohn.soft@gmail.com>
2022-07-04 16:46:35 +0000

    m

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
+
 def new_in_list(my_list, idx: int, element):
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx > len(my_list) - 1:
         return my_list
     else:
         another_list = [i for i in my_list]
